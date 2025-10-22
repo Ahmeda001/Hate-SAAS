febdbb97d423d7007e2ee78ad2cc81ab6b8c161f
Ahmed Ali <ahmed.a00185@gmail.com>
2025-10-22 18:15:01 +0500

    working api and app without ai

--- API/detection/models/lstm/__init__.py
+++ API/detection/models/lstm/__init__.py
@@ -11,14 +11,14 @@ from tensorflow.keras.models import load_model
 # Get the directory of the current script (models folder)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the full path to model.keras
-model_path = os.path.join(current_dir, 'model.keras')
+model_path = os.path.join(current_dir, 'best_model_updated.keras')
 model = load_model(model_path)
 
 
 # Load the tokenizer
 current_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the full path to tokenizer.pkl
-tokenizer_path = os.path.join(current_dir, 'tokenizer.pkl')
+tokenizer_path = os.path.join(current_dir, 'tokenizer_updated.pkl')
 
 # Load the tokenizer
 with open(tokenizer_path, 'rb') as f:
@@ -28,23 +28,19 @@ with open(tokenizer_path, 'rb') as f:
 def clean_text(text):
     text = text.lower()
     text = re.sub(r'http\S+|www\S+|https\S+|http\s+', '', text)
-    text = re.sub(r'\[.*?\]', '', text)
-    text = re.sub(r'\(.*?\)', '', text)
-    text = re.sub(r'\w*\d\w*', '', text)
+    text = re.sub(r'[\[\]\(\)]', '', text)
     text = re.sub(r'\s+', ' ', text)
     text = re.sub(r'\n', ' ', text)
     text = re.sub(r'\"+', '', text)
-    text = re.sub(r'“|”', '', text)
-    text = re.sub(r'’', '', text)
-    text = re.sub(r'&amp;', '', text)
-    text = re.sub(r'@[^\s]+', '', text)
-    text = re.sub(r'#[^\s]+', '', text)
+    text = re.sub(r'[“”’&]', '', text)
+    text = re.sub(r'@[^\s]+', 'user', text)
+    text = re.sub(r'#', '', text)
     text = re.sub(r'\brt\b', '', text)
     text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
 
-    # Replacing common abbreviations
-    replace_dict = {
+    abbreviations = {
         r'\bthx\b': 'thanks',
+        r'\bpm\b': '',
         r'\bpls\b': 'please',
         r'\bplz\b': 'please',
         r'\bbtw\b': 'by the way',
@@ -60,8 +56,9 @@ def clean_text(text):
         r'\bnvm\b': 'never mind',
         r'\brip\b': 'rest in peace'
     }
-    for pattern, repl in replace_dict.items():
+    for pattern, repl in abbreviations.items():
         text = re.sub(pattern, repl, text)
+
     return text
 
 def clean_demojized_text(text):
@@ -77,7 +74,7 @@ def preprocess_text(text, max_len=75):
 # Prediction function
 def predict_text(text):
     input_vector = preprocess_text(text)
-    prediction = model.predict(input_vector)[0]
+    prediction = model.predict(input_vector)[0][0]
 
     # If binary classification
     label = "Hate Speech Detected" if prediction >= 0.5 else "No Hate Speech"
